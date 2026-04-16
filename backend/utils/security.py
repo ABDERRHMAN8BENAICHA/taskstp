77098bef54cab3cd3a4dbcf13b6dc759bf408562
ben aicha abderrahmane <dahmangot2020@gmail.com>
2026-04-16 16:16:52 +0100

    fix cors

--- backend/utils/security.py
+++ backend/utils/security.py
@@ -3,7 +3,7 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"])
 
 def hash_password(password: str):
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
 
 def verify_password(password, hashed):
-    return pwd_context.verify(password, hashed)
+    return pwd_context.verify(password[:72], hashed)
