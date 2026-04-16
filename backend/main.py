1e3f6e5ca1993bb9016564bf40a757e37b36311f
ben aicha abderrahmane <dahmangot2020@gmail.com>
2026-04-16 16:08:30 +0100

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://taskstp.vercel.app/"],  # مؤقت (للتجربة فقط)
+    allow_origins=["https://taskstp.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
